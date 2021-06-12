4cd6fdff5f15607861a02cd373642426fc51eef1
Paul <paulantonioifrmi@yahoo.com>
2021-06-12 18:11:31 +0300

    first commit

---
+++ src/main/java/com/degree/studyitserver/StudyItServerApplication.java
@@ -0,0 +1,13 @@
+package com.degree.studyitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyItServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyItServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/degree/studyitserver/StudyItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degree.studyitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
